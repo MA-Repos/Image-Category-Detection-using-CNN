75eb6d5559426be4b5f7f74be92a88304760d57f
Mir Abbas <mir7abbas@gmail.com>
2019-08-04 20:37:54 -0400

    Model Creation

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ Created on Sun Aug  4 20:04:20 2019
 import keras
 from keras.datasets import cifar10
 from keras.utils import np_utils
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.layers import Dense, Dropout, Flatten, Activation, Conv2D, MaxPooling2D
 
 (xTrain, yTrain), (xTest, yTest) = cifar10.load_data()
 
@@ -25,3 +28,28 @@ xTest /= 255
 xTrain = np_utils.to_categorical(yTrain, 10)
 yTest = np_utils.to_categorical(yTest, 10)
 
+##################
+# Model Creation
+##################
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), activation='relu', padding='same', input_shape=xTrain.shape[1:]))
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256), activation='relu')
+model.add(Dropout(0.5))
+model.add(Dense(10), activation='softmax')
+
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = SGD(0.1),
+              metrics = ['accuracy'])
+
+print(model.summary)
